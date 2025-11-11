d8a2c21cc6b21f0c9acbcddb674450dbcd978d4e
Abhinav71205 <abhinavrawat712@gmail.com>
2025-11-11 22:23:24 +0530

    Update user_auth.c

--- DSA Codes/user_auth.c
+++ DSA Codes/user_auth.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "user_auth.h"
+
 void clean(char *str) {
     for (int i = 0; str[i]; i++) {
         if (str[i] == '\n' || str[i] == '\r')
@@ -11,19 +12,29 @@ void clean(char *str) {
 int signup_user() {
     char username[USERNAME_LEN], password[PASSWORD_LEN];
     printf("\n--- SIGN UP ---\n");
-    printf("Enter username: ");
+    printf("Enter new username: ");
     scanf("%s", username);
     printf("Enter password: ");
     scanf("%s", password);
-
-    FILE *f = fopen(USER_FILE, "a");
+    FILE *f = fopen(USER_FILE, "r");
+    if (f) {
+        char u[USERNAME_LEN], p[PASSWORD_LEN];
+        while (fscanf(f, "%s %s", u, p) == 2) {
+            if (strcmp(u, username) == 0) {
+                printf("Username already exists. Try another one.\n");
+                fclose(f);
+                return 0;
+            }
+        }
+        fclose(f);
+    }
+    f = fopen(USER_FILE, "a");
     if (!f) {
-        printf("Error opening file.\n");
+        printf("Error opening user file.\n");
         return 0;
     }
     fprintf(f, "%s %s\n", username, password);
     fclose(f);
-
     printf("Signup successful! You can now login.\n");
     return 1;
 }
@@ -34,27 +45,22 @@ int login_user(UserSession *session) {
     scanf("%s", username);
     printf("Enter password: ");
     scanf("%s", password);
-
     FILE *f = fopen(USER_FILE, "r");
     if (!f) {
-        printf("No user file found.\n");
+        printf("No users found. Please sign up first.\n");
         return 0;
     }
-
-    char file_user[USERNAME_LEN], file_pass[PASSWORD_LEN];
+    char u[USERNAME_LEN], p[PASSWORD_LEN];
     int found = 0;
-
-    while (fscanf(f, "%s %s", file_user, file_pass) == 2) {
-        clean(file_user);
-        clean(file_pass);
-
-        if (strcmp(file_user, username) == 0 && strcmp(file_pass, password) == 0) {
+    while (fscanf(f, "%s %s", u, p) == 2) {
+        clean(u);
+        clean(p);
+        if (strcmp(u, username) == 0 && strcmp(p, password) == 0) {
             found = 1;
             break;
         }
     }
     fclose(f);
-
     if (found) {
         strcpy(session->username, username);
         session->is_logged_in = 1;
